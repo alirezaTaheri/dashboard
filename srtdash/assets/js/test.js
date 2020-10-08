d50cca18068ab3db4cea776a857d7b68c568eee4
Alireza <alireza.taheri210@gmail.com>
2020-10-08 14:52:49 +0330

    Navbar Responsivness is really good

---
+++ srtdash/assets/js/test.js
@@ -0,0 +1,52 @@
+(function($) {
+    "use strict";
+
+    /*================================
+    sidebar collapsing
+    ==================================*/
+    if (window.innerWidth <= 1364) {
+        $('.page-container').addClass('sbar_collapsed');
+    }
+    $('.nav-btn').on('click', function() {
+        $('.page-container').toggleClass('sbar_collapsed');
+    });
+
+    /*================================
+    Start Footer resizer
+    ==================================*/
+    var e = function() {
+        var e = (window.innerHeight > 0 ? window.innerHeight : this.screen.height) - 5;
+        (e -= 67) < 1 && (e = 1), e > 67 && $(".main-content").css("min-height", e + "px")
+    };
+    $(window).ready(e), $(window).on("resize", e);
+
+    /*================================
+    sidebar menu
+    ==================================*/
+    $("#menu").metisMenu();
+
+    /*================================
+    slimscroll activation
+    ==================================*/
+    $('.menu-inner').slimScroll({
+        height: 'auto'
+    });
+
+    /*================================
+    stickey Header
+    ==================================*/
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop(),
+            mainHeader = $('#sticky-header'),
+            mainHeaderHeight = mainHeader.innerHeight();
+
+        // console.log(mainHeader.innerHeight());
+        if (scroll > 1) {
+            $("#sticky-header").addClass("sticky-menu");
+        } else {
+            $("#sticky-header").removeClass("sticky-menu");
+        }
+    });
+
+
+})(jQuery);
